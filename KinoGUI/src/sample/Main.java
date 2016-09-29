2ff319337dc5654b42e2755feceaeb4a10bd2910
usermango <bardurthomsen@hotmail.com>
2016-09-29 12:21:19 +0200

    ..

--- KinoGUI/src/sample/Main.java
+++ KinoGUI/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("View/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View/Film.fxml"));
         LoginSalMainmenuController.initializeController(primaryStage);
         primaryStage.setTitle("EsKino");
         primaryStage.setScene(new Scene(root, 1280, 820));
