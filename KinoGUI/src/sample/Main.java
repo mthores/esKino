675ab3a1ff2f03b1bd664f790005530ab5ccf96e
Mikkel Thore Simonsen <m.thore@hotmail.com>
2016-09-29 16:08:27 +0200

    Rettelse af GitFejl

--- KinoGUI/src/sample/Main.java
+++ KinoGUI/src/sample/Main.java
@@ -5,11 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 7346247e5bccb5accea671f842a5a2ea76e4329f
-
 import javafx.scene.control.Button;
 
 
@@ -24,29 +19,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-<<<<<<< HEAD
-=======
+        Parent root = FXMLLoader.load(getClass().getResource("View/login.fxml"));
 
->>>>>>> 7346247e5bccb5accea671f842a5a2ea76e4329f
-       // Parent root = FXMLLoader.load(getClass().getResource("View/Film.fxml"));
-
-
-        Parent root = FXMLLoader.load(getClass().getResource("View/AddMovie.fxml"));
-
-
-<<<<<<< HEAD
-
-        //Parent root = FXMLLoader.load(getClass().getResource("View/Film.fxml"));
-
-=======
->>>>>>> 7346247e5bccb5accea671f842a5a2ea76e4329f
         LoginSalMainmenuController.initializeController(primaryStage);
         primaryStage.setTitle("EsKino");
         primaryStage.setScene(new Scene(root, 800, 640));
         primaryStage.show();
 
-
-        //TableView<Object> tW3 = new TableView<>();
     }
 
 
